3b56c2a5fef660216f2635fb31845ac28935c213
sharaththota10 <sharathkumar.thota@zemosolabs.com>
2021-12-14 10:13:06 +0530

    initial commit

---
+++ 09-cruddemo-data-Rest/src/main/java/com/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.cruddemo.dao;
+
+import com.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+
+    // that's it .........no code
+
+}
